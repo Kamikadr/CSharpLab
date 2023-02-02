9e91058693f0ea469aeecccdc6e1d5e782d3f224
Kamikadr <60061947+Kamikadr@users.noreply.github.com>
2023-02-02 15:13:10 +0700

    Update count of stream

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -3,7 +3,9 @@
 Token token = new Token();
 Console.WriteLine("Enter message:");
 token.Data = Convert.ToString(Console.ReadLine());
+Console.WriteLine("Enter number of stream:");
+token.Recipient = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter TTL:");
 token.LifeTime = Convert.ToInt32(Console.ReadLine());
 
-await TaskManager.TokenRing(5, token);
+await TaskManager.TokenRing(token.Recipient, token);
